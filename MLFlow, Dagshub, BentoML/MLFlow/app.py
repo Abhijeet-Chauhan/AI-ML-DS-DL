f6fabf10376efd94f0b26accc145f1b4a7b301a4
Abhijeet Chauhan <62278049+Abhijeet-Chauhan@users.noreply.github.com>
2025-07-22 16:55:04 +0530

    added project 4

--- MLFlow, Dagshub, BentoML/MLFlow/app.py
+++ MLFlow, Dagshub, BentoML/MLFlow/app.py
@@ -55,7 +55,8 @@ if __name__ == "__main__":
 
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
     l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
-
+    import dagshub
+    dagshub.init(repo_owner='Abhijeet-Chauhan', repo_name='AI-ML-DS-DL', mlflow=True)
     with mlflow.start_run():
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
         lr.fit(train_x, train_y)
